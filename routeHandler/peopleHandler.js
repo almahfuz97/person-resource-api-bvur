5c39f4fd5f6dd5ad1233052af0c9de418b361f8d
almahfuz <almahfuz0327@gmail.com>
2023-01-11 00:37:55 +0600

    fixed error

--- routeHandler/peopleHandler.js
+++ routeHandler/peopleHandler.js
@@ -12,8 +12,13 @@ router.get('/', async (req, res) => {
             res.json({ message: err.message })
         }
         else {
-            res.status(200).json({ data: data })
+            if (data) {
+                res.status(200).json({ data: data })
+            }
+            else {
+                res.status(404).json({ message: 'Not found' })
 
+            }
         }
     }).clone()
 
@@ -26,7 +31,13 @@ router.get('/:id', async (req, res) => {
             res.json({ message: err.message })
         }
         else {
-            res.status(200).json({ data: data })
+            if (data) {
+                res.status(200).json({ data: data })
+            }
+            else {
+                res.status(404).json({ message: 'Not found' })
+
+            }
 
         }
     }).clone()
@@ -61,13 +72,19 @@ router.put('/:id', async (req, res) => {
         $set: {
             ...req.body
         }
-    }, (err) => {
+    }, (err, data) => {
+        console.log(data)
         if (err) {
             res.status(500).json({ message: err.message })
         }
         else {
-            res.status(200).json({ message: 'Person\'s info updated successfully' })
+            if (data) {
+                res.status(200).json({ data: data })
+            }
+            else {
+                res.status(404).json({ message: 'Not found' })
 
+            }
         }
     }).clone()
 
